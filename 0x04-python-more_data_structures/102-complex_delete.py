bf06ee5da07104560e9664da4f1b5fd2fdf973a9
Cyusaalain <umuhirecyusaa@gmail.com>
2023-11-08 05:05:00 -0800

    python5

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    list_keys list(a_dictionary.keys())
+    for value dic in list_keys:
+if value a dictionary.get(value_dic):
+    della dictionary[value_dic]
+    return (a_dictionary)
