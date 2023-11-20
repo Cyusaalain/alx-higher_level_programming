58cedb7afd946805b052425a3090bbc6718de329
Cyusaalain <umuhirecyusaa@gmail.com>
2023-11-20 00:25:21 -0800

    0x05

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        while count < x:
+            print(my_list[count], end="")
+            count += 1
+        print()
+        return count
+    except IndexError:
+        print()
+        return count
