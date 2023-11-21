0656f4d790be393135505472f1a754e297b78fe8
Cyusaalain <umuhirecyusaa@gmail.com>
2023-11-21 02:33:50 -0800

    0x06

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module documentation goes here.
+"""
+
+class Square:
+    """
+    Class documentation goes here.
+    """
+    pass
+
+"""
+Function documentation goes here.
+"""
+def my_function():
+    pass
